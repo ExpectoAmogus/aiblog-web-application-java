910a512c9a0f973384d6ad3dc4d13871b453c97d
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-04-22 01:59:39 +0200

    add carousel on homepage

--- frontend/aiblog-app/src/app/components/home/home.component.ts
+++ frontend/aiblog-app/src/app/components/home/home.component.ts
@@ -1,7 +1,48 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ArticlesService} from "../../services/articles.service";
+import {ArticleDTO} from "../../models/article";
+import {ImagesService} from "../../services/images.service";
 
 @Component({
   selector: 'app-home-content',
-  templateUrl: './home.component.html'
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent{}
+export class HomeComponent implements OnInit {
+  public articles: ArticleDTO[] = [];
+  public articleImages: { [articleId: number]: string } = {};
+
+  constructor(
+    private articlesService: ArticlesService,
+    private imagesService: ImagesService
+  ) {}
+
+  ngOnInit() {
+    this.getArticles();
+  }
+
+  getArticles(): void {
+    this.articlesService.getArticles().subscribe(articles => {
+      this.articles = articles.slice(0, 9);
+      for (const article of articles) {
+        this.imagesService
+          .getImage(article.uuid, 1)
+          .subscribe(imageUrl => {
+            this.articleImages[article.id] = imageUrl;
+          });
+      }
+    });
+  }
+
+  getArticleImage(articleId: number): string {
+    return this.articleImages[articleId];
+  }
+
+  truncateHTML(html: string, maxLength: number): string {
+    let truncatedText = html.slice(0, maxLength);
+    if (html.length > maxLength) {
+      truncatedText += '...';
+    }
+    return truncatedText;
+  }
+}
