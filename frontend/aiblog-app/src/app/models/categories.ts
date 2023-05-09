e4faa0015d3a4cce7f73652a3ea7430e50b0cb32
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-05-09 16:09:26 +0200

    add categories for filter

---
+++ frontend/aiblog-app/src/app/models/categories.ts
@@ -0,0 +1,6 @@
+export const CATEGORIES = [
+  { value: 'sport', viewValue: 'Sport' },
+  { value: 'business', viewValue: 'Business' },
+  { value: 'culture', viewValue: 'Culture' },
+  { value: 'entertainment', viewValue: 'Entertainment' }
+];
