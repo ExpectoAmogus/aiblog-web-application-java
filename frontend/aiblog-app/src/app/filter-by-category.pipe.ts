73f2e171e0a1b1757456bcd1d1e1c5b866d436a7
ExpectoAmogus <ExpectoAmogus@users.noreply.github.com>
2023-05-09 16:08:45 +0200

    add filter-by-category.pipe

---
+++ frontend/aiblog-app/src/app/filter-by-category.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import {ArticleDTO} from "./models/article";
+
+@Pipe({
+  name: 'filterByCategory'
+})
+export class FilterByCategoryPipe implements PipeTransform {
+
+  transform(articles: ArticleDTO[], category: string): ArticleDTO[] {
+    return articles.filter(article => article.category === category);
+  }
+
+}
