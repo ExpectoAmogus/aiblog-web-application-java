9807d5a3734aa3ccfeb34d566257b118e533219e
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2023-12-14 19:37:06 +0100

    METEOR alg

--- backend-python/meteor.py
+++ backend-python/meteor.py
@@ -1,6 +1,14 @@
+from nltk import word_tokenize
 from nltk.translate.meteor_score import meteor_score, single_meteor_score
+import nltk
+
+nltk.download('punkt')
+nltk.download('wordnet')
 
 
 def calculate_meteor(reference, candidate):
-    score = round(single_meteor_score(reference, candidate), 4)
+    reference_tokens = word_tokenize(reference.lower())
+    candidate_tokens = word_tokenize(candidate.lower())
+
+    score = round(single_meteor_score(reference_tokens, candidate_tokens), 4)
     return score
