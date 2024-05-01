35085e64983276a0253ff169efcf33a5dbe55163
Igor <72547132+ExpectoAmogus@users.noreply.github.com>
2024-05-01 15:13:34 +0200

    add requests for models, update parser

--- backend-python/Api/llms_response.py
+++ backend-python/Api/llms_response.py
@@ -1,5 +1,9 @@
 import os
 import pandas as pd
+import anthropic
+import cohere
+from openai import OpenAI
+import json
 
 from bleu import calculate_bleu
 from meteor import calculate_meteor
@@ -22,12 +26,12 @@ def evaluate_text_quality(reference, candidate):
 
 
 # Function for processing prompts and writing results to CSV files
-def process_prompts(prompt_data, model_name, request_func, reference_data):
+def process_prompts(prompt_data, model_name, request_func, reference_data, api_key):
     results = []
 
     for prompt in prompt_data['prompts']:
         reference = reference_data.loc[prompt_data['prompts'].index(prompt), 'article_text']
-        candidate = request_func(prompt)  # Getting candidate from the model
+        candidate = request_func(prompt, api_key)  # Getting candidate from the model
 
         evaluation_results = evaluate_text_quality(reference, candidate)
         results.append({'prompt': prompt.strip(), 'reference': reference, 'candidate': candidate,
@@ -40,24 +44,64 @@ def process_prompts(prompt_data, model_name, request_func, reference_data):
 
 
 # Function for requesting GPT-3.5 model API
-def request_gpt_3_5(prompt):
-    # Implementation to request GPT-3.5 model API
-    pass
+def request_gpt_3_5(prompt, api_key):
+    client = OpenAI(
+        api_key=api_key,
+    )
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a satirical article writer."},
+            {"role": "user", "content": prompt},
+        ]
+    )
+    return response.choices[0].message.content.replace('\n', '').replace('\r', '').replace('\t', '')
 
 
-def request_claude(prompt):
-    # Implementation to request Claude model API
-    pass
+def request_claude(prompt, api_key):
+    client = anthropic.Anthropic(
+        # api_key=os.environ.get("ANTHROPIC_API_KEY"),
+        api_key=api_key,
+    )
+    message = client.messages.create(
+        model="claude-3-opus-20240229",
+        max_tokens=1000,
+        temperature=0,
+        messages=[
+            {
+                "role": "user",
+                "content": [
+                    {
+                        "type": "text",
+                        "text": prompt
+                    }
+                ]
+            }
+        ]
+    )
+    return message.content[0].text.replace('\n', '').replace('\r', '').replace('\t', '')
 
 
-def request_cohere(prompt):
-    # Implementation to request Cohere model API
-    pass
+def request_cohere(prompt, api_key):
+    co = cohere.Client(
+        # api_key=os.environ.get("COHERE_API_KEY"),
+        api_key=api_key,
+    )
+
+    chat = co.chat(
+        message=prompt,
+        model="command"
+    )
+
+    return chat.text.replace('\n', '').replace('\r', '').replace('\t', '')
 
 
 # Loading prompts from each file
 prompt_files = ['prompts_1.txt', 'prompts_2.txt', 'prompts_3.txt']
 prompts_data = []
+
+with open('api_keys.json', 'r') as file:
+    api_keys = json.load(file)
 
 for prompt_file in prompt_files:
     with open(prompt_file, 'r', encoding='utf-8') as file:
@@ -68,6 +112,6 @@ reference_data = pd.read_csv('../parser_result.csv')
 
 # Processing prompts and writing results for each model
 for prompt_data in prompts_data:
-    process_prompts(prompt_data, 'gpt_3_5', request_gpt_3_5, reference_data)
-    process_prompts(prompt_data, 'claude', request_claude, reference_data)
-    process_prompts(prompt_data, 'cohere', request_cohere, reference_data)
+    process_prompts(prompt_data, 'gpt_3_5', request_gpt_3_5, reference_data, api_keys['openai_api_key'])
+    process_prompts(prompt_data, 'claude', request_claude, reference_data, api_keys['anthropic_api_key'])
+    process_prompts(prompt_data, 'cohere', request_cohere, reference_data, api_keys['cohere_api_key'])
--- backend-python/Parser/parse_babylonbee.py
+++ backend-python/Parser/parse_babylonbee.py
@@ -11,7 +11,7 @@ def parse_url(url):
         soup = BeautifulSoup(response.text, 'html.parser')
         p_tags = soup.select('#articleShowContent > div.flex.flex-col.bg-gray-50.rounded.shadow-md.overflow-hidden.mb'
                              '-4 > div.flex-1.bg-white.pt-0.sm\\:px-6.pb-4.flex.flex-col.justify-between > div > div p')
-        content = '\n'.join(tag.text.strip() for tag in p_tags)
+        content = ''.join(tag.text.strip() for tag in p_tags)
         return content.strip()
     except Exception as e:
         print(f"Error while reading URL {url}: {e}")
